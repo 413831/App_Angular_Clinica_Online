661e2432394ae129005b7116ac5639c427ab163d
413831 <alepleins3pp@gmail.com>
2020-06-04 18:49:37 -0300

    Fix deploy

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/angularapp'));
 
-// Configuracion para el mail
-// app.use(ra origin: "*" }));
-app.use(bodyParser.json());
+// // Configuracion para el mail
+// // app.use(ra origin: "*" }));
+// app.use(bodyParser.json());
 
 app.listen(process.env.PORT || 8080);
 
@@ -20,39 +20,39 @@ app.get('/*',function(req,res){
 });
 
 // define a sendmail endpoint, which will send emails and response with the corresponding status
-app.post("/sendmail", (req, res) => {
-  console.log("request came");
-  let user = req.body;
-  sendMail(user, (err, info) => {
-    if (err) {
-      console.log(err);
-      res.status(400);
-      res.send({ error: "Failed to send email" });
-    } else {
-      console.log("Email has been sent");
-      res.send(info);
-    }
-  });
-});
+// app.post("/sendmail", (req, res) => {
+//   console.log("request came");
+//   let user = req.body;
+//   sendMail(user, (err, info) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(400);
+//       res.send({ error: "Failed to send email" });
+//     } else {
+//       console.log("Email has been sent");
+//       res.send(info);
+//     }
+//   });
+// });
 
-const sendMail = (user, callback) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: "<sender email>",
-      pass: "<password>"
-    }
-  }); 
-}
+// const sendMail = (user, callback) => {
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//       user: "<sender email>",
+//       pass: "<password>"
+//     }
+//   }); 
+// }
 
-const mailOptions = {
-  from: `<No reply>`,
-  to: `<${usuario.email}>`,
-  subject: "<Message subject>",
-  html: "<h1>Esto es un mail autogenerado</h1>"
-};
+// const mailOptions = {
+//   from: `<No reply>`,
+//   to: `<${usuario.email}>`,
+//   subject: "<Message subject>",
+//   html: "<h1>Esto es un mail autogenerado</h1>"
+// };
 
 
 console.log('Finalizo');
