5009d0ab5ceb1194bbc4cfaaf01b131a22f4acd5
413831 <alepleins3pp@gmail.com>
2020-06-11 20:01:17 -0300

    Update turnos

--- src/app/pages/turnos/turnos.component.ts
+++ src/app/pages/turnos/turnos.component.ts
@@ -12,28 +12,37 @@ import { Especialidad } from 'src/app/clases/Medico';
 export class TurnosComponent implements OnInit {  
   public usuario: Usuario;
   public turnos: Turno[];
+  public campo: string;
+  public valor: any;
 
   constructor() 
   { 
-    this.usuario = Object.assign(new Usuario, JSON.parse(localStorage.getItem('usuario')));
-    this.turnos = (JSON.parse(localStorage.getItem('turnos'))
+    this.usuario = JSON.parse(localStorage.getItem('usuario'));
+    if(this.usuario)
+    {
+      this.usuario = Object.assign(new Usuario, this.usuario);
+    
+      this.turnos = (JSON.parse(localStorage.getItem('turnos'))
                       .filter( turno => 
                       {
                         this.usuario.nombre == turno.nombrePaciente ||
                         this.usuario.nombre == turno.nombreMedico
                       }))
                       .map( turno => Object.assign(new Turno, turno));
+    }
   }
 
   ngOnInit(): void {
-    // Reemplazar con Local Storage
-    // this.turnos = this.servicioTurnos.leer();
+    
     
   }
 
-  buscar()
+
+  filtrar()
   {
-    // Se filtran los turnos de la especialidad seleccionada y fecha y estado
-    // this.turnos ...
+    // Busco los turnos con el campo ingresado en el buscador
+    this.turnos = this.turnos.filter( () => Object.keys(this.turnos).includes(this.campo));
+    // Busco los turnos con el valor ingresado en el buscador
+    this.turnos = this.turnos.filter( () => Object.values(this.turnos).includes(this.valor));
   }
 }
