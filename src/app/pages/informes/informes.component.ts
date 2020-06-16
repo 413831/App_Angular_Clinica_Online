c164896cb1374d973e22375dfa2bd8e0608c5a0e
413831 <alepleins3pp@gmail.com>
2020-06-15 21:11:45 -0300

    Backup

--- src/app/pages/informes/informes.component.ts
+++ src/app/pages/informes/informes.component.ts
@@ -7,17 +7,19 @@ import { Usuario } from 'src/app/clases/Usuario';
 import { Sesion } from 'src/app/clases/Sesion';
 import { Turno, Dia } from 'src/app/clases/Turno';
 import { DiaAtencionPipe } from 'src/app/pipes/dia-atencion.pipe';
+import zipcelx from 'zipcelx';
+
 HC_exporting(Highcharts);
 
-interface Valor{
-   name : string,
-   data : any[]
+interface Valor {
+   name: string,
+   data: any[]
 }
 
 @Component({
-  selector: 'app-informes',
-  templateUrl: './informes.component.html',
-  styleUrls: ['./informes.component.css']
+   selector: 'app-informes',
+   templateUrl: './informes.component.html',
+   styleUrls: ['./informes.component.css']
 })
 export class InformesComponent implements OnInit {
    usuario: Usuario;
@@ -27,122 +29,166 @@ export class InformesComponent implements OnInit {
    horarios: string[] = Turno.horarios;
    highcharts = Highcharts;
    horas: Array<any> = [];
-   valoresHoras : Valor;
+   valoresHoras: Valor;
    labelsEspecialidades: string[] = [];
 
    data = [{
       name: 'Turnos',
       data: [15, 20, 10, 30, 17, 10]
-   },{
+   }, {
       name: 'Medicos',
       data: [13, 10, 9, 15, 16, 10]
    }];
 
-  constructor(private appService : AppService) 
-  {
+   constructor(private appService: AppService) {
       this.appService.getLoginData();
       this.usuario = JSON.parse(localStorage.getItem('usuario'));
 
-     if(this.usuario)
-     {
+      if (this.usuario) {
          this.usuario = Object.assign(new Usuario, this.usuario);
          console.log(JSON.parse(localStorage.getItem('sesiones')));
          this.sesiones = (JSON.parse(localStorage.getItem('sesiones'))
-                              .filter( sesion => this.usuario.id == sesion.idUsuario)
-                              .map( sesion => Object.assign(new Sesion, sesion)));      
+            .filter(sesion => this.usuario.id == sesion.idUsuario)
+            .map(sesion => Object.assign(new Sesion, sesion)));
          ///
          this.turnos = (JSON.parse(localStorage.getItem('turnos')))
-                             .map(turno => Object.assign(new Turno, turno));
-        
-         
+            .map(turno => Object.assign(new Turno, turno));
+
+
          this.crearGraficos();
-     }
+      }
    }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+   }
 
-  crearGraficos()
-  {
+   crearGraficos() {
       let horas: number[] = [];
 
-      this.sesiones.sort((a,b) => this.ordenarDias(a,b));
+      this.sesiones.sort((a, b) => this.ordenarDias(a, b));
       console.log(this.sesiones);
-      this.sesiones.forEach(sesion => {         
+      this.sesiones.forEach(sesion => {
          console.log(new Date(sesion.fechaInicio).getHours());
          console.log(`${new Date(sesion.fechaInicio).getDay()} - ${new Date(sesion.fechaInicio).getHours()}`);
          horas.push(new Date(sesion.fechaInicio).getHours());
       })
 
       this.valoresHoras = {
-         name : "Horas",
-         data : horas
+         name: "Horas",
+         data: horas
       }
-  }
+   }
 
 
-  configurarGrafico(XData : any, XTitle: string, YData: any, YTitle: string, data: any, titulo: string)
-  {
-  
-   let chartOptions = {   
-      chart: {
-         type: "spline"
-      },
-      title: {
-         text: titulo
-      },
-      xAxis:{
-         categories: XData
-      },
-      exporting:{
-         enabled: true,
-      },
-      yAxis: {          
-         title:{
-            text: YTitle
-         }, 
-         categories: YData
-      },
-      series: [data]
-    };
+   configurarGrafico(XData: any, XTitle: string, YData: any, YTitle: string, data: any, titulo: string) {
 
-    return chartOptions;
-  }
+      let chartOptions = {
+         chart: {
+            type: "areaspline"
+         },
+         title: {
+            text: titulo
+         },
+         xAxis: {
+            categories: XData
+         },
+         exporting: {
+            enabled: true,
+         },
+         yAxis: {
+            title: {
+               text: YTitle
+            },
+            categories: YData
+         },
+         tooltip: {
+            shared: true,
+            valueSuffix: ' units'
+          },
+          plotOptions: {
+            areaspline: {
+              fillOpacity: 0.5
+            }
+          },
+         series: [data]
+      };
 
-  ordenarDias(diaA : Sesion, diaB: Sesion)
-  {
-      if(new Date(diaA.fechaInicio).getDay() > new Date(diaB.fechaInicio).getDay())
-      {
+      return chartOptions;
+   }
+
+   ordenarDias(diaA: Sesion, diaB: Sesion) {
+      if (new Date(diaA.fechaInicio).getDay() > new Date(diaB.fechaInicio).getDay()) {
          return 1;
       }
-      else if(new Date(diaA.fechaInicio).getDay() < new Date(diaB.fechaInicio).getDay())
-      {
+      else if (new Date(diaA.fechaInicio).getDay() < new Date(diaB.fechaInicio).getDay()) {
          return -1;
       }
-      else
-      {
+      else {
          return 0;
       }
-  }
+   }
 
-  calcularOperaciones()
-  {
-   // Obtengo las diferentes especialidades para mostrar
-   this.turnos.forEach(turno => {
-      if(!this.labelsEspecialidades.includes(turno.especialidad))
-      {
-         this.labelsEspecialidades.push(turno.especialidad);
-      }
-   });
+   calcularOperaciones() {
+      // Obtengo las diferentes especialidades para mostrar
+      this.turnos.forEach(turno => {
+         if (!this.labelsEspecialidades.includes(turno.especialidad)) {
+            this.labelsEspecialidades.push(turno.especialidad);
+         }
+      });
+   }
 
-
-
-  }
-   
-    
-   
-    
-    
-    
+//    (function (H) {
+//       if (window.zipcelx && H.getOptions().exporting) {
+//           H.Chart.prototype.downloadXLSX = function () {
+//               var div = document.createElement('div'),
+//                   name,
+//                   xlsxRows = [],
+//                   rows;
+//               div.style.display = 'none';
+//               document.body.appendChild(div);
+//               rows = this.getDataRows(true);
+//               xlsxRows = H.map(rows.slice(1), function (row) {
+//                   return H.map(row, function (column) {
+//                       return {
+//                           type: typeof column === 'number' ? 'number' : 'string',
+//                           value: column
+//                       };
+//                   });
+//               });
+  
+//               // Get the filename, copied from the Chart.fileDownload function
+//               if (this.options.exporting.filename) {
+//                   name = this.options.exporting.filename;
+//               } else if (this.title && this.title.textStr) {
+//                   name = this.title.textStr.replace(/ /g, '-').toLowerCase();
+//               } else {
+//                   name = 'chart';
+//               }
+  
+//               window.zipcelx({
+//                   filename: name,
+//                   sheet: {
+//                       data: xlsxRows
+//                   }
+//               });
+//           };
+  
+//           // Default lang string, overridable in i18n options
+//           H.getOptions().lang.downloadXLSX = 'Download XLSX';
+  
+//           // Add the menu item handler
+//           H.getOptions().exporting.menuItemDefinitions.downloadXLSX = {
+//               textKey: 'downloadXLSX',
+//               onclick: function () {
+//                   this.downloadXLSX();
+//               }
+//           };
+  
+//           // Replace the menu item
+//           var menuItems = H.getOptions().exporting.buttons.contextButton.menuItems;
+//           menuItems[menuItems.indexOf('downloadXLS')] = 'downloadXLSX';
+//       }
+  
+//   }(Highcharts));
 
 }
