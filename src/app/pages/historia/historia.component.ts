b438b090813c2e07a74be7f160da72a25d27ed17
413831 <alepleins3pp@gmail.com>
2020-06-18 18:44:19 -0300

    Falta detalle historia

--- src/app/pages/historia/historia.component.ts
+++ src/app/pages/historia/historia.component.ts
@@ -19,17 +19,19 @@ export class HistoriaComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private router: Router) {
     this.route.params.subscribe(params => this.idPaciente = params['paciente']);
-    this.paciente = (JSON.parse(localStorage.getItem('pacientes'))
-                        .filter(paciente => this.idPaciente == paciente.id))
-                        .map(paciente => Object.assign(new Paciente, paciente))[0];
-    this.historia = JSON.parse(localStorage.getItem('historias'))
-                        .filter(historia => historia.idPaciente == this.idPaciente)
-                        .map(historia => Object.assign(new Historia, historia))[0];
-
-    console.log(this.paciente);
+  
   }
 
   ngOnInit(): void {
+    this.paciente = (JSON.parse(localStorage.getItem('pacientes'))
+    .filter(paciente => this.idPaciente == paciente.id))
+    .map(paciente => Object.assign(new Paciente, paciente))[0];
+    this.historia = JSON.parse(localStorage.getItem('historias'))
+        .filter(historia => historia.idPaciente == this.idPaciente)
+        .map(historia => Object.assign(new Historia, historia))[0];
+
+    console.log(this.paciente);
+    console.log(this.historia);
     // Si hay historia obtengo los datos
     if (this.historia != undefined) 
     {
@@ -44,13 +46,13 @@ export class HistoriaComponent implements OnInit {
 
             this.turnos.forEach(turno => 
               {
-              if (Object.keys(turno) == dato) 
-              {
-                console.log(turno[dato]);
-                this.datos.push({
-                  key: dato,
-                  value: turno[dato]
-                });
+                if (Object.keys(turno) == dato) 
+                {
+                  console.log(turno[dato]);
+                  this.datos.push({
+                    key: dato,
+                    value: turno[dato]
+                  });
               }
           });
         })
