fd62ffbd8a8238e4c8193eb1b9da6b4952d6d83a
413831 <alepleins3pp@gmail.com>
2020-06-18 20:53:05 -0300

    Can Activate

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,24 +2,29 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../servicios/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NotificacionComponent } from '../componentes/notificacion/notificacion.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(
-    public authService: AuthService,
-    public router: Router
-  ){ }
+  constructor(private _snackBar: MatSnackBar,public router: Router)
+  { }
 
   canActivate( next: ActivatedRouteSnapshot,
                state: RouterStateSnapshot): Observable<boolean | UrlTree> |  Promise<boolean | UrlTree> |
                                             boolean | UrlTree
   {
-    if(this.authService.isLoggedIn !== true) 
+    let usuario = localStorage.getItem('usuario');
+
+    if(!usuario) 
     {
-      window.alert('Acceso denegado, Inicie sesión.')
+      this._snackBar.openFromComponent(NotificacionComponent, {
+        duration: 3 * 1000,
+        data: "Se modificó el turno exitosamente."
+      });
       this.router.navigate(['login'])
     }
     return true;
