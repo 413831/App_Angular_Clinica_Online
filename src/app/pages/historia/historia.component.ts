29e87c3cd3ba5b952e1c837731ca2beae10fbac7
413831 <alepleins3pp@gmail.com>
2020-06-23 14:29:01 -0300

    Historia clinica

--- src/app/pages/historia/historia.component.ts
+++ src/app/pages/historia/historia.component.ts
@@ -3,6 +3,7 @@ import { Historia } from 'src/app/clases/Historia';
 import { Paciente } from 'src/app/clases/Paciente';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Turno } from 'src/app/clases/Turno';
+import { MiservicioService } from 'src/app/servicios/miservicio.service';
 
 @Component({
   selector: 'app-historia',
@@ -10,56 +11,61 @@ import { Turno } from 'src/app/clases/Turno';
   styleUrls: ['./historia.component.css']
 })
 export class HistoriaComponent implements OnInit {
-  idPaciente: string;
+  public idPaciente: string;
+  public panelOpenState = true;
   public paciente: Paciente;
+  public imgSrc: string;
   public historia: Historia;
   public turnos: Turno[];
   public datosAdicionales = [];
   public datos = [];
 
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(private route: ActivatedRoute, private router: Router) 
+  {
     this.route.params.subscribe(params => this.idPaciente = params['paciente']);
-  
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
     this.paciente = (JSON.parse(localStorage.getItem('pacientes'))
-                        .filter(paciente => this.idPaciente == paciente.id))
-                        .map(paciente => Object.assign(new Paciente, paciente))[0];
+                          .filter(paciente => this.idPaciente == paciente.id))
+                          .map(paciente => Object.assign(new Paciente, paciente))[0];
     this.historia = JSON.parse(localStorage.getItem('historias'))
                         .filter(historia => historia.idPaciente == this.idPaciente)
                         .map(historia => Object.assign(new Historia, historia))[0];
+    MiservicioService.descargarImagen(this.paciente.imagen)
+                     .then( ()=> this.imgSrc = MiservicioService.imgSrc);
 
     console.log(this.paciente);
     console.log(this.historia);
-    // Si hay historia obtengo los datos
-    if (this.historia != undefined) 
-    {
+    this.obtenerExtras();
+  }
+
+  obtenerExtras()
+  {
+     // Si hay historia obtengo los datos
+    if (this.historia != undefined) {
       this.turnos = this.historia.consultas;
       this.datosAdicionales = this.historia.adicionales;
 
       if (this.datosAdicionales) 
       {
+        this.datosAdicionales.forEach(dato => {
 
-        this.datosAdicionales.forEach(dato => 
-          {
-
-            this.turnos.forEach(turno => 
-              {
-                if (Object.keys(turno) == dato) 
-                {
-                  console.log(turno[dato]);
-                  this.datos.push({
-                    key: dato,
-                    value: turno[dato]
-                  });
-              }
+          this.turnos.forEach(turno => {
+            if (Object.keys(turno).includes(dato)) {
+              console.log(turno[dato]);
+              this.datos.push({
+                key: dato,
+                value: turno[dato]
+              });
+            }
           });
         })
+        console.log("Datos adicionales");
         console.log(this.datos);
       }
     }
-
   }
 
 }
